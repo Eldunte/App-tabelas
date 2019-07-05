57e9b79dcf81e299857002b2e8d02ae74340ff19
Eldunte <51166414+Eldunte@users.noreply.github.com>
2019-07-05 09:15:26 -0300

    Recomeçando Js do zero e adicionando botões

--- js/main.js
+++ js/main.js
@@ -1,22 +1,4 @@
-var pecas = document.querySelectorAll(".Peças");
-
-for (var index = 0; index < pecas.length; index++) {
-    const peca = jogos[index];
- 
-    var colunanota = jogo.querySelector(".info-nota");
-   
-    pecas.querySelector("info-nome") = cooler.nome;
-
-    var cooler = {
-        nome:"cooler",
-         
-    }
-
+function adicionarTable() {
     
-   
-    
-    
-    
-
     
 }
