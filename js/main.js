51c7b3d444eea030386fa46e1d8725422d1c495e
Eldunte <51166414+Eldunte@users.noreply.github.com>
2019-07-05 09:50:18 -0300

    Criando uma função de adicionar nomes
    criando também uma variável que puxa o inputNome.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,20 @@
-function adicionarTable() {
+var inputNome = document.querySelector("#pecaN")
+
+
+pecasNome = [
+    "cooler"
+]
+
+console.log(inputNome);
+
+function adicionarTableNome() {
+    for (nome of pecasNome) {
+    var colunaNome = document.createElement('td');
+    var textoNome = document.createTextNode(pecasNome);
+    
+    colunaNome.appendChild()
+      
+    }
     
     
 }
