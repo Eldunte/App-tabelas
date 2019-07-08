f99539268d450f724185db7fafec2d958204d096
Eldunte <51166414+Eldunte@users.noreply.github.com>
2019-07-08 12:39:29 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
-var tableNome= document.querySelector("#NomedaPeca")
-var tableValor = document.querySelector("#")
-var tableFuncao = //pega a id, ta na id da Th
+var tableNome = document.querySelector("#NomedaPeca")
+var tableValor = document.querySelector("#NumerodoValor")
+var tableFuncao = document.querySelector("#Funcao")
 
-var inputNome = document.querySelector("#pecaN")
+var inputNome = document.querySelector("#pecaN");
+var inputValor = document.querySelector("#NumerodoValor");
+var inputFuncao = document.querySelector("#Funcao")
 
-console.log(tableNome)
+console.log(inputFuncao);
+console.log(inputValor);
+console.log(inputNome);
 
 
 
@@ -12,6 +16,13 @@ var pecasNome = [
     "cooler"
 ]
 
+var pecasValor = [
+    "1273"
+]
+
+var pecasFuncao = [
+    "não deixar o processador esquentar muito"
+]
 
 
 
@@ -21,21 +32,67 @@ var pecasNome = [
 
 function RendezirarTableNome() {
     for (nome of pecasNome) {
-        tableNome.innerHTML = pecasNome
 
-       
+
+
 
         var colunaNome = document.createElement('td')
         var TextoN = document.createTextNode(nome);
 
         colunaNome.appendChild(TextoN);
         tableNome.appendChild(colunaNome);
-        
-       
+
+
 
     }
 
 
 }
 
-RendezirarTableNome();
+function RendezirarTableValor() {
+    for (valor of pecasValor) {
+
+        var colunaValor = document.createElement('td');
+        var TextoV = document.createTextNode(valor);
+
+        colunaValor.appendChild(TextoV);
+        tableValor.appendChild(colunaValor);
+
+
+
+    }
+
+
+}
+
+function RendezirarTableFuncao() {
+    for (Funcao of pecasFuncao) {
+
+
+
+
+        var colunaFuncao = document.createElement('td')
+        var TextoF = document.createTextNode(Funcao);
+
+        colunaFuncao.appendChild(TextoF);
+        tableFuncao.appendChild(colunaFuncao);
+
+
+
+    }
+
+
+}
+
+function AdicionarPeca() {
+    var nomeText = inputNome.value
+    pecasNome.push(nomeText);
+    inputNome.value = "";
+    RendezirarTableNome();
+
+}
+
+
+
+RendezirarTableFuncao();
+RendezirarTableValor();
